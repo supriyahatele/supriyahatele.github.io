85be158c973fb5eb5ac0aae3c86ed7d2d4fca835
supriyahatele <niyahatele70@gmail.com>
2025-06-04 19:41:31 +0530

    changes made

--- script.js
+++ script.js
@@ -105,47 +105,47 @@ function scrollUp() {
 window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
+// const themeButton = document.getElementById('theme-button')
+// const darkTheme = 'dark-theme'
+// const iconTheme = 'uil-sun'
+// // Previously selected topic (if user selected)
+// const selectedTheme = localStorage.getItem('selected-theme')
 
-const selectedIcon = localStorage.getItem('selected-icon')
+// const selectedIcon = localStorage.getItem('selected-icon')
 
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+// // We obtain the current theme that the interface has by validating the dark-theme class
+// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
 // We validate if the user previously chose a topic
-if (selectedTheme) {
-    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+// if (selectedTheme) {
+//     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+//     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
     
-    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
-}
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
+//     themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+// }
+// // Activate / deactivate the theme manually with the button
+// themeButton.addEventListener('click', () => {
     
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
+//     // Add or remove the dark / icon theme
+//     document.body.classList.toggle(darkTheme)
 
-    themeButton.classList.toggle(iconTheme)
+//     themeButton.classList.toggle(iconTheme)
 
 
 
 
 
     
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
+//     // We save the theme and the current icon that the user chose
+//     localStorage.setItem('selected-theme', getCurrentTheme())
 
-    localStorage.setItem('selected-icon', getCurrentIcon())
+//     localStorage.setItem('selected-icon', getCurrentIcon())
 
 
-})
+// })
 
 // slider
 
@@ -262,13 +262,13 @@ img4.src = imageArr4[index4++];
 
 document.getElementById('resume-button-1').addEventListener('click', function() {
   window.open(
-    "https://drive.google.com/file/d/1kBGD7tjtC2g3Cb2RbcqyKQJSxcR8eT0t/view?usp=sharing"
+    "https://drive.google.com/file/d/1_GSXfrQfGGpG-XVRBqWf71VYIz7x88wQ/view?usp=sharing"
   );
 });
 
 document.getElementById('resume-button-2').addEventListener('click', function() {
   window.open(
-    "https://drive.google.com/file/d/1kBGD7tjtC2g3Cb2RbcqyKQJSxcR8eT0t/view?usp=sharing"
+    "https://drive.google.com/file/d/1_GSXfrQfGGpG-XVRBqWf71VYIz7x88wQ/view?usp=sharing"
 );
 });
 
